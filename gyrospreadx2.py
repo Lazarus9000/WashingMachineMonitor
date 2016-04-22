c923f924ac093203a6d8ba7fabace224d74fa0dc
Lars Knudsen <jegharen@gmail.com>
2016-04-22 19:32:59 +0200

    Added code to read MCP3008
    Found on
    https://learn.adafruit.com/reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/script

--- gyrospreadx2.py
+++ gyrospreadx2.py
@@ -11,6 +11,10 @@ from oauth2client.client import SignedJwtAssertionCredentials
 
 import smbus
 import math
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+DEBUG = 1
 
 #setup for PB
 API_KEY = 'o.4j3lgYYD14vc62HIjYmZ5m8y37ReC6bh'
@@ -24,6 +28,60 @@ push = pb.NotePush('Hello world!', 'The little pi that could')
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
 
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)
+ 
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+ 
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+ 
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+ 
+        GPIO.output(cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS = 25
+ 
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+ 
+# 10k trim pot connected to adc #0
+potentiometer_adc = 0;		
+
+# read the analog pin
+trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
+		
 def try_io(call, tries=10):
 	assert tries > 0
 	error = None
@@ -36,6 +94,7 @@ def try_io(call, tries=10):
 		except IOError as e:
 			error = e
 			tries -= 1
+			time.sleep(1)
 			print 'Failed - tries remaining: ', tries
 		else:
 			break
