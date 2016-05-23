3cbb497d7bbcb62b1f4d990b4fdf024a2a8c6161
Lars Knudsen <jegharen@gmail.com>
2016-05-23 21:54:20 +0200

    absolute path

--- gyrospreadx2.py
+++ gyrospreadx2.py
@@ -13,6 +13,8 @@ import smbus
 import math
 import RPi.GPIO as GPIO
 
+DIR=/home/pi/FTP/git/WashingMachineMonitor
+
 GPIO.setmode(GPIO.BCM)
 DEBUG = 1
 
@@ -160,7 +162,7 @@ bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
 ##prevZ2 = try_io(lambda: read_word_2c(address2, 0x3f))
 
 time.sleep(1)
-GDOCS_OAUTH_JSON       = 'MyProject-43a6520ce94c.json'
+GDOCS_OAUTH_JSON       = $DIR/MyProject-43a6520ce94c.json
 
 status = "off"
 
